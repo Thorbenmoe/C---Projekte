b4846f440c44ed37de94736bffa166328daea4a3
thorben möller <thorben.moe@t-online.de>
2025-08-28 19:03:20 +0200

    commit
    commit

--- Basics/Arrays/example2/Arrays2.cc
+++ Basics/Arrays/example2/Arrays2.cc
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <cstdint>
 #include <windows.h>
+#include <array>
 
 // -------------------------------------------------------------
 // Hilfsfunktion: Konsole leeren
 // -------------------------------------------------------------
+
+//--------------------------------------------------------------
+// globals
+//--------------------------------------------------------------
+constexpr static auto num_rows = std::uint32_t{3};
+constexpr static auto num_cols = std::uint32_t{2};
+
 void clearConsole()
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -26,18 +34,68 @@ void clearConsole()
     SetConsoleCursorPosition(hConsole, homeCoords);
 }
 
+//--------------------------------------------------------------
+// Arrays summieren
+//--------------------------------------------------------------
+int arraysum(int matrix1[num_rows][num_cols], int matrix_col_sum[num_cols])
+{
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            matrix_col_sum[j] += matrix1[i][j];
+        }
+    }
+    return 0;
+}
+
+//--------------------------------------------------------------
+// Arrays printen
+//--------------------------------------------------------------
+int printArray(int matrix_col_sum[])
+{
+    for (int k = 0; k < num_cols; k++)
+    {
+        std::cout << matrix_col_sum[k] << "\n";
+    }
+}
+
+//--------------------------------------------------------------
+//double Array funktion
+//--------------------------------------------------------------
+double dbl_array_sum(double dbl_array[], const std::size_t length)
+{
+    for (int i = 0; i < length; i++)
+    {
+       
+        
+            dbl_array[i]=dbl_array[i]+i;
+            dbl_array[i]=dbl_array[i]+dbl_array[i-1];
+            std::cout << dbl_array[i] << "\n";
+        
+        
+    }
+    
+    return 0;
+
+}
+
 int init()
 {
     clearConsole();
-    constexpr std::uint32_t len = 4;
-    std::uint32_t array1[len] = {12, 32, 203, 14};
-    std::uint32_t reverseArray[len];
-    
-    for(std::uint32_t i =0; i < len; i++)
-    {
-        reverseArray[i]=array1[len-i-1];
-        std::cout << reverseArray[i] << std::endl;
-    }
+
+
+    // 2d Array (Matrix)
+    // 3 Zeilen 2 Spalten
+
+    int matrix1[num_rows][num_cols] = {1, 2, 3, 4, 5, 6};
+    double dbl_array[99];
+    int matrix_col_sum[num_cols] = {};
+
+    arraysum(matrix1, matrix_col_sum);
+    dbl_array_sum(dbl_array,99);
+    printArray(matrix_col_sum);
 
     return 0;
 }
