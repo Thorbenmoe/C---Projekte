b266256f2d77efe047ce2778e5ecda98969885c7
thorben möller <thorben.moe@t-online.de>
2025-08-28 22:01:15 +0200

    commit

--- Basics/Pointer/Pointer.cc
+++ Basics/Pointer/Pointer.cc
@@ -3,8 +3,12 @@
 #include <windows.h>
 #include <array>
 
+
+//& Memory Adress
+//*p Wert der Memory Adress
 int init()
 {   
+
     int num =5;
     int* p= &num;
     std::cout <<"Test " << p<< "\n";
